94a28ec7160377ab3247e4432a71d7ca74feae77
Min <74136649+DANGHYEON@users.noreply.github.com>
2023-09-08 22:22:02 +0900

    jpa 공부 - 주문 서비스 테스트2

--- src/test/java/jpabook/jpashop/service/OrderServiceTest.java
+++ src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -7,6 +7,7 @@ import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.exception.NotEnougStockException;
 import jpabook.jpashop.repository.MemberRepository;
 import jpabook.jpashop.repository.OrderRepository;
 import org.junit.jupiter.api.Test;
@@ -38,15 +39,10 @@ class OrderServiceTest {
     @Test
     public void 상품주문() throws Exception{
 
-        Member member = new Member();
-        member.setName("회원1");
-        member.setAddress(new Address("서울","강가","123-123"));
-        em.persist(member);
+        Member member = getMember();
 
-        Book book = new Book();
-        book.setName("시골 jpa");
-        book.setPrice(10000);
-        book.setStockQuantity(10);
+
+        Book book = getBook();
 
         int orderCount = 2;
         Long orderId = orderService.order(member.getId(), book.getId(), orderCount);
@@ -54,17 +50,59 @@ class OrderServiceTest {
         Order getOrder = orderRepository.findOne(orderId);
 
         assertEquals( OrderStatus.ORDER,getOrder.getStatus());
+        assertEquals(1,getOrder.getOrderItemList().size());
+        assertEquals(1000 * orderCount,getOrder.getTotalPrice());
+        assertEquals(8,book.getStockQuantity());
+
+    }
+
+    private Book getBook() {
+        Book book = new Book();
+        book.setName("시골 jpa");
+        book.setPrice(1000);
+        book.setStockQuantity(10);
+        em.persist(book);
+        return book;
+    }
+
+    private Member getMember() {
+        Member member = new Member();
+        member.setName("회원1");
+        member.setAddress(new Address("서울","강가","123-123"));
+        em.persist(member);
+        return member;
+    }
+
+    @Test
+    public void 상품주문_재고수량초과() throws Exception{
+        Member member = getMember();
+        Item item = getBook();
+
+        int orderCount = 11;
+
+        assertThrows(NotEnougStockException.class,() ->
+                orderService.order(member.getId(), item.getId(), orderCount));
 
     }
 
     @Test
     public void 주문취소() throws Exception{
 
+        Member member = getMember();
+        Item item = getBook();
+
+        int orderCount = 2;
+
+        Long orderId = orderService.order(member.getId(),item.getId(),orderCount);
+
+        orderService.cancleOrder(orderId);
+
+        Order getOrder = orderRepository.findOne(orderId);
+
+        assertEquals(OrderStatus.CANCEL,getOrder.getStatus());
+        assertEquals(10,item.getStockQuantity());
+
     }
 
-    @Test
-    public void 상품주문_재고수량초과() throws Exception{
-
-    }
 
 }
